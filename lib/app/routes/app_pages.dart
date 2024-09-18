ef25c1951ac76e5c984081acaa3318fb01cc2c93
Venita Thaithae <64160264@go.buu.ac.th>
2024-09-18 18:40:40 +0700

    del const register

--- lib/app/routes/app_pages.dart
+++ lib/app/routes/app_pages.dart
@@ -57,7 +57,7 @@ class AppPages {
     ),
     GetPage(
       name: _Paths.REGISTER,
-      page: () => const RegisterView(),
+      page: () =>  RegisterView(),
       binding: RegisterBinding(),
     ),
     GetPage(
