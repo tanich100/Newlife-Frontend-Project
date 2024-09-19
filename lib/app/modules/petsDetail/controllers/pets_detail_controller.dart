a4f9858adf58c7874bf861ac015555f505ec607d
Venita Thaithae <64160264@go.buu.ac.th>
2024-09-19 17:04:31 +0700

    Mockup UI Detail Animal

--- lib/app/modules/petsDetail/controllers/pets_detail_controller.dart
+++ lib/app/modules/petsDetail/controllers/pets_detail_controller.dart
@@ -4,6 +4,18 @@ class PetsDetailController extends GetxController {
   //TODO: Implement PetsDetailController
 
   final count = 0.obs;
+
+  final  isFavorite = false.obs;
+
+  void toggleFavorite() {
+    isFavorite.value = !isFavorite.value;
+  }
+
+  void sendAdoptionRequest() {
+    // Implement the logic for sending an adoption request
+    print('Adoption request sent');
+    Get.snackbar('Success', 'Adoption request sent successfully');
+  }
   @override
   void onInit() {
     super.onInit();
--- lib/app/modules/petsDetail/views/pets_detail_view.dart
+++ lib/app/modules/petsDetail/views/pets_detail_view.dart
@@ -1,22 +1,199 @@
 import 'package:flutter/material.dart';
-
 import 'package:get/get.dart';
 
-import '../controllers/pets_detail_controller.dart';
-
-class PetsDetailView extends GetView<PetsDetailController> {
+class PetsDetailView extends StatelessWidget {
   const PetsDetailView({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('PetsDetailView'),
-        centerTitle: true,
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back),
+          onPressed: () => Get.offNamed('/home'),
+        ),
       ),
-      body: const Center(
+      body: SingleChildScrollView(
+        child: Padding(
+          padding:  EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              _buildPetImage(),
+              SizedBox(height: 20,),
+              Padding(
+                padding: EdgeInsets.all(16.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                   _buildPetTypeTag(),
+                    SizedBox(height: 16),
+                    _buildPetInfoHeader(),
+                    SizedBox(height: 16),
+                    _buildPetLocation(),
+                    SizedBox(height: 20),
+                    _buildPetDescription(),
+                    SizedBox(height: 80),
+                    _buildControls(),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildPetImage() {
+    return Center(
+      child: Container(
+        width: 370,
+        height: 350,
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(20),
+          child: Image(
+            image: NetworkImage(
+              'https://p16-va.lemon8cdn.com/tos-alisg-v-a3e477-sg/8120a94612554f8dac3c25f48ca214e7~tplv-tej9nj120t-origin.webp',
+            ),
+            fit: BoxFit.cover,
+          ),
+        ),
+      ),
+    );
+  }
+
+ Widget _buildPetTypeTag() {
+  return Row(
+    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+    children: [
+      Row(
+        children: [
+          _buildTagBox(Icons.pets, 'แมว'),
+          SizedBox(width: 10),
+          _buildTagBox(Icons.female, 'เมีย'),
+        ],
+      ),
+       Container(
+        padding: EdgeInsets.all(8),
+        decoration: BoxDecoration(
+          color:  Color.fromARGB(255, 239, 190, 31),
+          shape: BoxShape.circle,
+        ),
+        child: Icon(Icons.favorite, color: Colors.red, size: 30),
+      ),
+    ],
+  );
+}
+
+Widget _buildTagBox(IconData icon, String text) {
+  return Container(
+    width: 120, 
+    height: 40,  
+    padding: EdgeInsets.symmetric(vertical: 8, horizontal: 12),
+    decoration: BoxDecoration(
+      borderRadius: BorderRadius.circular(8),
+      border: Border.all(
+        color: Colors.black,
+        width: 0.5,
+      ),
+    ),
+    child: Row(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Icon(icon, size: 26), // ลดขนาด icon ลงเล็กน้อย
+        SizedBox(width: 4),
+        Expanded(
+          child: Text(
+            text,
+            style: TextStyle(fontWeight: FontWeight.w800, fontSize: 14),
+          textAlign: TextAlign.center,
+            overflow: TextOverflow.ellipsis,
+          ),
+        ),
+      ],
+    ),
+  );
+}
+
+ Widget _buildPetInfoHeader() {
+  return Row(
+    children: [
+      Text(
+        'โลร่า',
+        style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+      ),
+      SizedBox(width: 10,),
+      Text(
+        '(8 เดือน)',
+        style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+      ),
+      SizedBox(width: 20), 
+      Container(
         child: Text(
-          'PetsDetailView is working',
-          style: TextStyle(fontSize: 20),
+          'สถานะ:',
+          style: TextStyle(fontSize: 18, color: Colors.black,fontWeight: FontWeight.bold),
+          
+        ),
+      ),
+      SizedBox(width: 10,),
+      Text(
+          'พร้อมให้รับอุปการะ',
+          style: TextStyle(fontSize: 14, color: Colors.black87),
+        ),
+      
+    ],
+  );
+}
+
+ 
+
+  Widget _buildPetLocation() {
+    return Row(
+      children: [
+        Icon(Icons.location_on_sharp,color: Colors.red.shade500, size: 25),
+        SizedBox(width: 4),
+        Expanded(
+          child: Text('คอนโดภูเก็ตทาวน์ ต.รัษฎา อ.เมืองภูเก็ต จ.ภูเก็ต'),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildPetDescription() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          'รายละเอียด',
+          style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+        ),
+        SizedBox(height: 20,),
+        Text(
+          'น้องเป็นแมวที่ขี้อ้อนมาก เป็นแมวที่ชอบเล่นขี้เล่นพอสมควรอยู่ไม่นิ่งเลยค่ะ ชอบกินขนมแมวมาก ชอบเป็นที่รักคนและขี้อ้อนมาก ๆ',
+        ),
+      ],
+    );
+  }
+
+  Widget _buildControls() {
+    return Center(
+      child: SizedBox(
+        width: 250,
+        height: 60,
+        child: ElevatedButton(
+          child: Text(
+            'ต้องการอุปการะ',
+            style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+          ),
+          onPressed: () {},
+          style: ElevatedButton.styleFrom(
+            backgroundColor: const Color.fromARGB(255, 244, 204, 47),
+            foregroundColor: Colors.black,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(18),
+            ),
+          ),
         ),
       ),
     );
