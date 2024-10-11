4b1bfd9d147ce1575729d2bda95871a3eb5f602c
Pharadon Sirijhan <64160142@go.buu.ac.th>
2024-10-11 12:56:42 +0700

    userController get local user

--- lib/app/modules/user/controllers/user_controller.dart
+++ lib/app/modules/user/controllers/user_controller.dart
@@ -1,23 +1,101 @@
 import 'package:get/get.dart';
+import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
 
 class UserController extends GetxController {
-  //TODO: Implement UserController
+  static const int _version = 1;
+  static const String _dbName = "user.db";
+  RxInt userId = (-1).obs;
+  RxString userName = ''.obs;
+  RxString profileImage = ''.obs;
 
-  final count = 0.obs;
-  @override
-  void onInit() {
-    super.onInit();
+  Future<Database> get database async {
+    return openDatabase(
+      join(await getDatabasesPath(), _dbName),
+      version: _version,
+    );
   }
 
-  @override
-  void onReady() {
-    super.onReady();
+  Future<void> executeRawQuery(String query) async {
+    Database db = await database;
+    await db.execute(query);
   }
 
-  @override
-  void onClose() {
-    super.onClose();
+  Future<List<Map<String, dynamic>>> selectRawQuery(String query) async {
+    Database db = await database;
+
+    try {
+      List<Map<String, dynamic>> result = await db.rawQuery(query);
+      return result;
+    } catch (e) {
+      print('Error executing raw query: $e');
+      return [];
+    }
   }
 
-  void increment() => count.value++;
+  Future<void> closeDatabase() async {
+    Database db = await database;
+    await db.close();
+  }
+
+  static Future<Database> _getDB() async {
+    return openDatabase(
+      join(await getDatabasesPath(), _dbName),
+      onCreate: (db, version) async {
+        //สร้างตารางเก็บข้อมูล User
+        // มี User Id name image
+        await db.execute("""
+          CREATE TABLE device (
+            user_id INTEGER NOT NULL,
+            name TEXT NOT NULL,
+            image_url TEXT NOT NULL,
+            PRIMARY KEY (user_id)
+          );
+        """);
+
+        await db.execute("""
+            INSERT INTO device (user_id, name, image_url)
+            VALUES (?, ?, ?);
+          """, [-1, "", ""]);
+      },
+      version: _version,
+    );
+  }
+
+  Future<void> getUser() async {
+    try {
+      List<Map<String, dynamic>> result = await selectRawQuery("""
+        SELECT user_id, name, image_url 
+        FROM device 
+        WHERE user_id = 1;
+      """);
+
+      if (result.isNotEmpty) {
+        userId.value = result[0]['user_id'];
+        userName.value = result[0]['name'];
+        profileImage.value = result[0]['image_url'];
+      } else {
+        userId.value = -1;
+        userName.value = '';
+        profileImage.value = '';
+      }
+    } catch (e) {
+      print("Error fetching user: $e");
+    }
+  }
 }
+
+// @override
+// void onInit() {
+//   super.onInit();
+// }
+
+// @override
+// void onReady() {
+//   super.onReady();
+// }
+
+// @override
+// void onClose() {
+//   super.onClose();
+// }
