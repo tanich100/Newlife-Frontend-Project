e1229d231e818be4eb3a5e3cca39f2145468cca6
Pharadon Sirijhan <64160142@go.buu.ac.th>
2024-10-30 21:13:51 +0700

    modify back button result image search

--- lib/app/modules/camera/views/result_view.dart
+++ lib/app/modules/camera/views/result_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:newlife_app/app/modules/camera/views/result_widget.dart';
 
 class ResultView extends StatefulWidget {
@@ -14,6 +15,15 @@ class _ResultViewState extends State<ResultView> {
     return Scaffold(
       appBar: AppBar(
         title: const Text('ผลการค้นหาจากภาพ'),
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back,
+              color: Colors.black), // Custom back button icon
+          onPressed: () async {
+            // Get.toNamed('/home');
+            Get.back();
+            Get.back();
+          },
+        ),
         backgroundColor: Color.fromARGB(255, 239, 190, 31),
       ),
       body: Container(
